cef78adb0230e7272cf633ad5d143fa6324fbdef
ItayKos <itaykostika358@gmail.com>
2025-10-31 20:12:20 +0200

    Added Utils functionality - test successful

--- 2/Utils.cpp
+++ 2/Utils.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 #include "Utils.h"
-
-// This file is just for compilation at the moment, and does not work as required.
+#include "Stack.h"
 
 void reverse(int* nums, unsigned int size)
 {
-    int x = 0;
+    Stack* stack = new Stack;
+    initStack(stack);
+    int i = 0;
+    for (i = 0; i < size; i++)
+    {
+        push(stack, (unsigned int)(nums[i]));
+    }
+    for (i = 0; i < size; i++)
+    {
+        nums[i] = pop(stack);
+    }
+    cleanStack(stack);
+    delete stack;
 }
 
 int* reverse10()
 {
-    int x = 0;
-    int* y = &x;
-    return y;
+    int* arr = new int[SIZE_ARRAY];
+    int i = 0;
+    int temp = 0;
+    Stack* stack = new Stack;
+    initStack(stack);
+    for (i = 0; i < SIZE_ARRAY; i++)
+    {
+        std::cin >> temp;
+        push(stack, (unsigned int)temp);
+    }
+    for (i = 0; i < SIZE_ARRAY; i++)
+    {
+        arr[i] = pop(stack);
+    }
+    cleanStack(stack);
+    delete stack;
+    return arr;
 }
--- 2/Utils.h
+++ 2/Utils.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#define SIZE_ARRAY 10
+
 void reverse(int* nums, unsigned int size);
 int* reverse10();
 
